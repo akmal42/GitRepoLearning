69a9abd978fde106bb76c0fe91fc5ef1350e30b9
Akmal Hussain <akmalhussain0072@gmail.com>
2022-10-09 11:03:10 +0530

    3 changes

--- B.java
+++ B.java
@@ -3,6 +3,9 @@ public class B
 	public static void main(String [] args)
 	{
 		System.out.println("hello world from B");
+		System.out.println("Changes");
+		System.out.println("3 changes");
+
 	}
 
 }
